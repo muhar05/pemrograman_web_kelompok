56ac82558fc5068fbc5eabcac3c3a7c4b922518a
muhar05 <ferdiansyahmuh5@gmail.com>
2025-11-26 22:29:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+
+// Tombol kembali ke atas
+const backToTop = document.getElementById("backToTop");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
+
+backToTop.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
